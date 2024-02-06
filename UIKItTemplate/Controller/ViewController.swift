e852bd3b21cd4f9d2343406ecc0c7cb0bbe9a9c8
Dmitry Lorents <zmey.task@gmail.com>
2024-02-06 09:26:37 +0300

    Add swiftformat

--- UIKItTemplate/Controller/ViewController.swift
+++ UIKItTemplate/Controller/ViewController.swift
@@ -1,14 +1,10 @@
-//
-//  ViewController.swift
-//  UIKItTemplate
-//
-//  Created by Dmitry on 05.02.2024.
-//
+// ViewController.swift
+// Copyright © RoadMap. All rights reserved.
 
 import UIKit
+
 /// Class Controller
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
---
+++ UIKItTemplate/Model/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  UIKItTemplate
+//
+//  Created by Dmitry on 06.02.2024.
+//
+
+import Foundation
