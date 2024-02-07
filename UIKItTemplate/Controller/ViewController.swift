013c35706de344f36a443c82a61744265600192d
Dmitry Lorents <zmey.task@gmail.com>
2024-02-07 23:41:34 +0300

    Add nameAlert

--- UIKItTemplate/Controller/ViewController.swift
+++ UIKItTemplate/Controller/ViewController.swift
@@ -8,14 +8,16 @@ class ViewController: UIViewController {
     // MARK: - Constants
 
     let upperViewInset: CGFloat = 55
+    let labelHeight: CGFloat = 82
 
     // MARK: - Private Properties
 
-    private var backgroundImageView: UIImageView {
+    private lazy var imageViewBackground: UIImageView = {
         let imageView = UIImageView(frame: UIScreen.main.bounds)
         imageView.image = UIImage(named: "backgroundImage")
+        imageView.clipsToBounds = true
         return imageView
-    }
+    }()
 
     private lazy var guessNumberButton: UIButton = {
         let button = UIButton(frame: CGRect(x: 82, y: 264, width: 150, height: 150))
@@ -50,7 +52,13 @@ class ViewController: UIViewController {
     }()
 
     private lazy var greetingLabel: UILabel = {
-        let label = UILabel(frame: CGRect(x: 0, y: upperViewInset, width: UIScreen.main.bounds.width, height: 82))
+        let label = UILabel(frame: CGRect(
+            x: 0,
+            y: upperViewInset,
+
+            width: UIScreen.main.bounds.width,
+            height: labelHeight
+        ))
         label.font = UIFont.boldSystemFont(ofSize: 30)
         label.textAlignment = .center
         label.numberOfLines = 0
@@ -58,8 +66,8 @@ class ViewController: UIViewController {
         label.textColor = .white
         label.layer.borderColor = UIColor(white: 1, alpha: 0.8).cgColor
         label.layer.borderWidth = 3
+        label.isHidden = true
         label.text = "Test\ntext"
-
         return label
     }()
 
@@ -70,16 +78,34 @@ class ViewController: UIViewController {
         setViews()
     }
 
-    // MARK: - Public Methods
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showGreetingAlert()
+    }
 
     // MARK: - Private Methods
 
     private func setViews() {
         view.backgroundColor = .systemBackground
-        view.addSubview(backgroundImageView)
+        view.addSubview(imageViewBackground)
         view.addSubview(guessNumberButton)
         view.addSubview(calculatorButton)
         view.addSubview(greetingLabel)
+    }
+
+    private func showGreetingAlert() {
+        let alert = UIAlertController(title: "Пожалуйста, представьтесь", message: nil, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Готово", style: .default) { _ in
+            let nameText = alert.textFields?.first?.text ?? "No name"
+            self.updateAppearance(nameText)
+        }
+
+        alert.addAction(alertAction)
+        alert.addTextField { textField in
+            textField.placeholder = "Введите ваше имя"
+        }
+
+        present(alert, animated: true)
     }
 
     @objc private func guessButtonAction() {
@@ -88,5 +114,16 @@ class ViewController: UIViewController {
 
     @objc private func calculatorButtonAction() {
         print(#function)
+    }
+
+    private func updateAppearance(_ nameText: String) {
+        UIView.animate(withDuration: 0.3) {
+            self.imageViewBackground.frame.origin.y = self.upperViewInset + self.labelHeight
+        } completion: { _ in
+            UIView.animate(withDuration: 0.3) {
+                self.greetingLabel.isHidden = false
+                self.greetingLabel.text = "Приветствую,\n\(nameText.uppercased())"
+            }
+        }
     }
 }
