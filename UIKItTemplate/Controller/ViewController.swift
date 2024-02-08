dbc4e66573d4c528ce92de3c1ebc45eaa4050241
Dmitry Lorents <zmey.task@gmail.com>
2024-02-08 22:34:57 +0300

    Finish second task

--- UIKItTemplate/Controller/ViewController.swift
+++ UIKItTemplate/Controller/ViewController.swift
@@ -4,16 +4,8 @@
 import UIKit
 
 /// Class Controller
-class ViewController: UIViewController {
-    // MARK: - Constants
-
+final class ViewController: UIViewController {
     // MARK: - Private Properties
-
-    private var inputString = "" {
-        didSet {
-            print(inputString)
-        }
-    }
 
     private lazy var startButton: UIButton = {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: view.bounds.width - 40, height: 44))
@@ -27,17 +19,13 @@ class ViewController: UIViewController {
         return button
     }()
 
-    private lazy var topLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Вы ввели слово"
-        label.font = UIFont.boldSystemFont(ofSize: 16)
-        label.textAlignment = .center
-        label.sizeToFit()
-        label.center.x = view.center.x
-        label.frame.origin.y = 106
-        label.textColor = .black
-        return label
-    }()
+    private lazy var topLabel: UILabel = createBoldLabel(text: "Вы ввели слово", yPosition: 106)
+    private lazy var bottomLabel = createBoldLabel(
+        text: "А вот что получится, если читать справа налево",
+        yPosition: 282
+    )
+    private lazy var topWordLabel = createItalicLabel(yPosition: 163)
+    private lazy var bottomWordLabel = createItalicLabel(yPosition: 339)
 
     // MARK: - Life Cycle
 
@@ -49,9 +37,58 @@ class ViewController: UIViewController {
     // MARK: - Private Methods
 
     private func setViews() {
-        view.backgroundColor = .white // .systemBackground
+        view.backgroundColor = .systemBackground
         view.addSubview(startButton)
         view.addSubview(topLabel)
+        view.addSubview(bottomLabel)
+        view.addSubview(topWordLabel)
+        view.addSubview(bottomWordLabel)
+    }
+
+    private func createBoldLabel(text: String, yPosition: CGFloat) -> UILabel {
+        createLabel(
+            text: text,
+            color: .black,
+            font: UIFont.boldSystemFont(ofSize: 16),
+            yPosition: yPosition
+        )
+    }
+
+    private func createItalicLabel(yPosition: CGFloat) -> UILabel {
+        createLabel(
+            text: "",
+            color: .gray,
+            font: .italicSystemFont(ofSize: 16),
+            yPosition: yPosition
+        )
+    }
+
+    private func createLabel(text: String, color: UIColor, font: UIFont, yPosition: CGFloat) -> UILabel {
+        let label = UILabel(frame: CGRect(x: 0, y: yPosition, width: 275, height: 57))
+        label.text = text
+        label.font = font
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.center.x = view.center.x
+        label.textColor = .black
+        label.isHidden = true
+        return label
+    }
+    
+    private func showAllLabels(inputText: String) {
+        let animationDuration: CGFloat = 0.5
+        UIView.animate(withDuration: animationDuration) {
+            self.startButton.frame.origin.y = 598
+        } completion: { _ in
+            UIView.animate(withDuration: animationDuration) {
+                self.topLabel.isHidden = false
+                self.topWordLabel.text = inputText.capitalized
+                self.topWordLabel.isHidden = false
+                self.bottomLabel.isHidden = false
+                self.bottomWordLabel.text = String(Array(inputText).reversed()).capitalized
+                self.bottomWordLabel.isHidden = false
+            }
+        }
     }
 
     @objc private func showWordAlert() {
@@ -61,7 +98,8 @@ class ViewController: UIViewController {
         }
         let cancelAction = UIAlertAction(title: "Отмена", style: .cancel)
         let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
-            self.inputString = alert.textFields?.first?.text ?? ""
+            let inputText = alert.textFields?.first?.text ?? ""
+            self.showAllLabels(inputText: inputText)
         }
         alert.addAction(cancelAction)
         alert.addAction(okAction)
