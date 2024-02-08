b84e5ff4427aac0715fcba6d57618d838fca22da
Dmitry Lorents <zmey.task@gmail.com>
2024-02-08 21:05:52 +0300

    Add top label

--- UIKItTemplate/Controller/ViewController.swift
+++ UIKItTemplate/Controller/ViewController.swift
@@ -9,6 +9,12 @@ class ViewController: UIViewController {
 
     // MARK: - Private Properties
 
+    private var inputString = "" {
+        didSet {
+            print(inputString)
+        }
+    }
+
     private lazy var startButton: UIButton = {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: view.bounds.width - 40, height: 44))
         button.center = view.center
@@ -19,6 +25,18 @@ class ViewController: UIViewController {
         button.layer.cornerRadius = 12
         button.addTarget(self, action: #selector(showWordAlert), for: .touchUpInside)
         return button
+    }()
+
+    private lazy var topLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Вы ввели слово"
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textAlignment = .center
+        label.sizeToFit()
+        label.center.x = view.center.x
+        label.frame.origin.y = 106
+        label.textColor = .black
+        return label
     }()
 
     // MARK: - Life Cycle
@@ -33,9 +51,20 @@ class ViewController: UIViewController {
     private func setViews() {
         view.backgroundColor = .white // .systemBackground
         view.addSubview(startButton)
+        view.addSubview(topLabel)
     }
 
     @objc private func showWordAlert() {
-        print(#function)
+        let alert = UIAlertController(title: "Введите ваше слово", message: nil, preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.placeholder = "Введите слово"
+        }
+        let cancelAction = UIAlertAction(title: "Отмена", style: .cancel)
+        let okAction = UIAlertAction(title: "Ok", style: .default) { _ in
+            self.inputString = alert.textFields?.first?.text ?? ""
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
