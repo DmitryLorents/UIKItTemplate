2a0a18b477d76931c2e0bc3b31ea6ecc39002a92
Dmitry Lorents <zmey.task@gmail.com>
2024-02-08 13:51:30 +0300

    Add start button

--- UIKItTemplate/Controller/ViewController.swift
+++ UIKItTemplate/Controller/ViewController.swift
@@ -5,8 +5,37 @@ import UIKit
 
 /// Class Controller
 class ViewController: UIViewController {
+    // MARK: - Constants
+
+    // MARK: - Private Properties
+
+    private lazy var startButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: view.bounds.width - 40, height: 44))
+        button.center = view.center
+        button.backgroundColor = UIColor(named: "customGreenColor")
+        button.setTitle("Начать", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.titleLabel?.textColor = .white
+        button.layer.cornerRadius = 12
+        button.addTarget(self, action: #selector(showWordAlert), for: .touchUpInside)
+        return button
+    }()
+
+    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setViews()
+    }
+
+    // MARK: - Private Methods
+
+    private func setViews() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(startButton)
+    }
+
+    @objc private func showWordAlert() {
+        print(#function)
     }
 }
