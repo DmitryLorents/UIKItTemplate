de9d0c0c8c9092f2e0def867fb8845e2abe81b93
Dmitry Lorents <zmey.task@gmail.com>
2024-02-09 19:44:59 +0300

    Add imageView

--- UIKItTemplate/Controller/ViewController.swift
+++ UIKItTemplate/Controller/ViewController.swift
@@ -5,8 +5,40 @@ import UIKit
 
 /// Class Controller
 class ViewController: UIViewController {
+    // MARK: - Constants
+
+    private lazy var birthdayImageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 70, width: 125, height: 125))
+        imageView.center.x = view.center.x
+        imageView.image = UIImage(named: "birthdayLogoImage")
+        return imageView
+    }()
+
+    // MARK: - IBOutlets
+
+    // MARK: - Visual Components
+
+    // MARK: - Public Properties
+
+    // MARK: - Private Properties
+
+    // MARK: - Initializers
+
+    // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setUI()
+    }
+
+    // MARK: - Public Methods
+
+    // MARK: - IBAction
+
+    // MARK: - Private Methods
+
+    private func setUI() {
+        view.backgroundColor = .systemBackground
+        view.addSubview(birthdayImageView)
     }
 }
