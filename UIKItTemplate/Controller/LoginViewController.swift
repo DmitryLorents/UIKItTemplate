db9a71e2ea4e5bf63e3054dc7d37eff127633a14
Dmitry Lorents <zmey.task@gmail.com>
2024-02-09 21:35:36 +0300

    Add textFields

--- UIKItTemplate/Controller/LoginViewController.swift
+++ UIKItTemplate/Controller/LoginViewController.swift
@@ -33,6 +33,19 @@ class LoginViewController: UIViewController {
     private let signInLabel: UILabel = .createRedLabel(size: 26, text: "Sign in", yPosition: 266)
     private let emailLabel: UILabel = .createRedLabel(size: 16, text: "Email", yPosition: 318)
     private let passwordLabel: UILabel = .createRedLabel(size: 16, text: "Password", yPosition: 393)
+    private lazy var loginButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 20, y: 671, width: UIScreen.main.bounds.width - 40, height: 44))
+        button.backgroundColor = UIColor(named: "customRedColor")
+        button.layer.cornerRadius = 12
+        button.setTitle("Login", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont(name: "Verdana-bold", size: 16)
+        button.addTarget(nil, action: #selector(moveToBirthdayVC), for: .touchUpInside)
+        return button
+    }()
+
+    private let emailTextField: UITextField = .createCustomTextField(placeholder: "Typing email", yPosition: 347)
+    private let passwordTextField: UITextField = .createCustomTextField(placeholder: "Typing password", yPosition: 422)
 
     // MARK: - Life Cycle
 
@@ -41,12 +54,24 @@ class LoginViewController: UIViewController {
         setUI()
     }
 
-    // MARK: - Public Methods
-
     // MARK: - Private Methods
 
     private func setUI() {
         view.backgroundColor = .systemBackground
-        view.addSubViews(birthdayImageView, birthdayLabel, signInLabel, emailLabel, passwordLabel)
+        view.addSubViews(
+            birthdayImageView,
+            birthdayLabel,
+            signInLabel,
+            emailLabel,
+            passwordLabel,
+            loginButton,
+            emailTextField,
+            passwordTextField
+        )
+    }
+
+    @objc private func moveToBirthdayVC() {
+        let birthdayVC = BirthdayViewController()
+        navigationController?.pushViewController(birthdayVC, animated: true)
     }
 }
