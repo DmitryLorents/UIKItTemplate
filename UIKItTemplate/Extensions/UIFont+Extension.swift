b3fed041319356dcbae5016a37cc3429d0931fa7
Dmitry Lorents <zmey.task@gmail.com>
2024-02-14 22:16:47 +0300

    Add UIFont extension

---
+++ UIKItTemplate/Extensions/UIFont+Extension.swift
@@ -0,0 +1,11 @@
+// UIFont+Extension.swift
+// Copyright © RoadMap. All rights reserved.
+
+import UIKit
+
+/// Add fonts, corresponding to app
+public extension UIFont {
+    static func makeVerdanaBold14() -> UIFont? {
+        UIFont(name: "Verdana-bold", size: 14)
+    }
+}
