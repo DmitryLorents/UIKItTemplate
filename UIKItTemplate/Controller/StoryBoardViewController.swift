dfa4a9755c9f28183927667b753e3dc0eaeb322e
Dmitry Lorents <zmey.task@gmail.com>
2024-02-24 20:31:03 +0300

    Add corner radius

--- UIKItTemplate/Controller/StoryBoardViewController.swift
+++ UIKItTemplate/Controller/StoryBoardViewController.swift
@@ -4,4 +4,33 @@
 import UIKit
 
 /// Screen to show how storyboards constraints are working
-class StoryBoardViewController: UIViewController {}
+class StoryBoardViewController: UIViewController {
+    // MARK: - IBOutlets
+
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+
+    // MARK: - Life Cycle
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        roundSubViews()
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        roundSubViews()
+    }
+
+    // MARK: - Private Methods
+
+    private func roundSubViews() {
+        print(#function)
+        print(redView.frame.width)
+        print(redView.layer.cornerRadius)
+        redView.layer.cornerRadius = redView.frame.width / 4
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+    }
+}
