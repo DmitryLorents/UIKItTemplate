e79d6698f29d1514016c372281d0d6f09c548ca8
Dmitry Lorents <zmey.task@gmail.com>
2024-02-24 22:34:36 +0300

    Finish anchor constraints

--- UIKItTemplate/Controller/AnchorViewController.swift
+++ UIKItTemplate/Controller/AnchorViewController.swift
@@ -11,6 +11,9 @@ class AnchorViewController: UIViewController {
         static let minVertical: CGFloat = 30
         static let minHorizintal: CGFloat = 107
         static let maxValue: CGFloat = 1000
+        static let subViewVertical: CGFloat = 15
+        static let subViewHorizintal: CGFloat = 25
+        static let subViewInterItem: CGFloat = 8
     }
 
     // MARK: - Visual Components
@@ -27,6 +30,18 @@ class AnchorViewController: UIViewController {
         setupUI()
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        roundSubViews()
+    }
+
+    override func willTransition(
+        to newCollection: UITraitCollection,
+        with coordinator: UIViewControllerTransitionCoordinator
+    ) {
+        roundSubViews()
+    }
+
     // MARK: - Private Methods
 
     private func setupUI() {
@@ -39,40 +54,50 @@ class AnchorViewController: UIViewController {
         subviews.forEach { view.addSubview($0) }
         setupConstraints()
     }
+
+    private func roundSubViews() {
+        print(#function)
+        print(redView.frame.width)
+        print(redView.layer.cornerRadius)
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+    }
 }
 
 private extension AnchorViewController {
     func setupConstraints() {
         setupBlackViewConstraints()
+        setupYellowViewConstraints()
+        setupRedViewConstraints()
+        setupGreenViewConstraints()
     }
 
     func setupBlackViewConstraints() {
-        blackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        blackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        NSLayoutConstraint.activate([
+            blackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            blackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
 
-        let topMin = blackView.topAnchor.constraint(
-            greaterThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor,
-            constant: Inset.minVertical
-        )
-        topMin.isActive = true
+            blackView.topAnchor.constraint(
+                greaterThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor,
+                constant: Inset.minVertical
+            ),
 
-        let verticalMax = blackView.topAnchor.constraint(
-            lessThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor,
-            constant: Inset.maxValue
-        )
-        verticalMax.isActive = true
+            blackView.topAnchor.constraint(
+                lessThanOrEqualTo: view.safeAreaLayoutGuide.topAnchor,
+                constant: Inset.maxValue
+            ),
 
-        let horizontalMin = blackView.leadingAnchor.constraint(
-            greaterThanOrEqualTo: view.safeAreaLayoutGuide.leadingAnchor,
-            constant: 107
-        )
-        horizontalMin.isActive = true
+            blackView.leadingAnchor.constraint(
+                greaterThanOrEqualTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: Inset.minHorizintal
+            ),
 
-        let horizontalMax = blackView.leadingAnchor.constraint(
-            lessThanOrEqualTo: view.safeAreaLayoutGuide.leadingAnchor,
-            constant: Inset.maxValue
-        )
-        horizontalMax.isActive = true
+            blackView.leadingAnchor.constraint(
+                lessThanOrEqualTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: Inset.maxValue
+            ),
+        ])
 
         let height = blackView.heightAnchor.constraint(equalToConstant: Inset.maxValue)
         height.priority = .defaultLow
@@ -81,5 +106,33 @@ private extension AnchorViewController {
         let width = blackView.widthAnchor.constraint(equalToConstant: Inset.maxValue)
         width.priority = .defaultLow
         width.isActive = true
+    }
+
+    func setupYellowViewConstraints() {
+        NSLayoutConstraint.activate([
+            yellowView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            yellowView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            yellowView.heightAnchor.constraint(equalTo: yellowView.widthAnchor),
+            yellowView.leadingAnchor.constraint(equalTo: blackView.leadingAnchor, constant: Inset.subViewHorizintal),
+        ])
+    }
+
+    func setupRedViewConstraints() {
+        NSLayoutConstraint.activate([
+            redView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            redView.heightAnchor.constraint(equalTo: yellowView.heightAnchor),
+            redView.heightAnchor.constraint(equalTo: redView.widthAnchor),
+            redView.topAnchor.constraint(equalTo: blackView.topAnchor, constant: Inset.subViewVertical),
+            redView.bottomAnchor.constraint(equalTo: yellowView.topAnchor, constant: -Inset.subViewInterItem)
+        ])
+    }
+
+    func setupGreenViewConstraints() {
+        NSLayoutConstraint.activate([
+            greenView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            greenView.heightAnchor.constraint(equalTo: yellowView.heightAnchor),
+            greenView.heightAnchor.constraint(equalTo: greenView.widthAnchor),
+            greenView.topAnchor.constraint(equalTo: yellowView.bottomAnchor, constant: Inset.subViewInterItem),
+        ])
     }
 }
