265bbcb5459f9ec5cd7be0feef0583239de0f4ae
Dmitry Lorents <zmey.task@gmail.com>
2024-02-24 22:51:27 +0300

    Remove roundSubview from storyboard viewController

--- UIKItTemplate/Controller/StoryBoardViewController.swift
+++ UIKItTemplate/Controller/StoryBoardViewController.swift
@@ -10,27 +10,4 @@ class StoryBoardViewController: UIViewController {
     @IBOutlet var redView: UIView!
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenView: UIView!
-
-    // MARK: - Life Cycle
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        roundSubViews()
-    }
-
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        super.viewWillTransition(to: size, with: coordinator)
-        roundSubViews()
-    }
-
-    // MARK: - Private Methods
-
-    private func roundSubViews() {
-        print(#function)
-        print(redView.frame.width)
-        print(redView.layer.cornerRadius)
-        redView.layer.cornerRadius = redView.frame.width / 4
-        yellowView.layer.cornerRadius = yellowView.frame.width / 2
-        greenView.layer.cornerRadius = greenView.frame.width / 2
-    }
 }
